676c57f9b245bc7f09c704718d4991d639fcf695
PhongTa2511 <2k4phong@gmail.com>
2025-12-04 11:47:52 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: Theme.of(context).textTheme.head,
             ),
           ],
         ),
