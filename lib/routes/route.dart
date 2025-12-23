0ae9fce32bb490ffeb6989d949a611f73487509d
Ph11 <phongphu7015@gmail.com>
2025-12-23 23:08:14 +0700

    Phu: Add guided routes file

--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -6,6 +6,9 @@ import '../views/setting_page.dart';
 import '../views/parent_page.dart';
 import 'package:flutter/material.dart';
 
+// Mặc định đường dẫn / sẽ ở trang login
+// nếu muốn đi đến đường đẫn khác thì thêm /#/tên_đường_dẫn
+// Vd: http:localhost:4321/#/parent sẽ đến trang bố mẹ
 final Map<String, WidgetBuilder> appRoutes = {
   '/': (context) => const LoginScreen(),
   '/signup': (context) => const RegisterScreen(),
