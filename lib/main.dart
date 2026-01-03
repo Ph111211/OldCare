8bfcfb27f6c5388320f01bafcb32897ae810e195
Ph11 <phongphu7015@gmail.com>
2026-01-03 09:55:30 +0700

    Phu: config file main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'routes/route.dart';
 import 'firebase_options.dart';
 import 'auth_gate.dart';
 
